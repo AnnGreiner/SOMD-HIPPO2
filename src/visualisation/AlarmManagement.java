572bf037df5b70067e18f501484b768b1c93a42c
Ann-Kathrin Greiner-Perth <ak.greiner.perth@gmail.com>
2021-06-27 09:01:08 +0200

    alarm set up

--- src/visualisation/AlarmManagement.java
+++ src/visualisation/AlarmManagement.java
@@ -1,5 +1,21 @@
 package visualisation;
+import java.util.Timer;
 
 public class AlarmManagement {
+	
+
+	Timer timer= new Timer();
+	
+	public void startAlarm() {
+		
+	}
+	
+	public void snooze() {
+		
+	}
+	
+	public void resetAlarm() {
+		
+	}
 
 }
